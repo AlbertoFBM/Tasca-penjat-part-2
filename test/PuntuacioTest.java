e51ac50f6374b1b6180e29f6f1ca77b82b5a4c58
AlbertoFBM <74379840+AlbertoFBM@users.noreply.github.com>
2021-03-03 14:22:31 +0100

    Add files via upload

---
+++ test/PuntuacioTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import com.jaume.penjat.Puntuacio;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PuntuacioTest {
+
+    private Puntuacio puntuacio;
+
+    @Test
+    void calcularPuntuacio(){
+        assertEquals();
+    }
+
+}
