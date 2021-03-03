7facf4b1a41be8721ad70386d87bb517c203f04b
Alberto <aserranor@cifpfbmoll.eu>
2021-03-03 17:22:12 +0100

    Second commit. He fet mostrarDificultat()

--- test/PuntuacioTest.java
+++ test/PuntuacioTest.java
@@ -1,16 +1,27 @@
 package test;
-
 import com.jaume.penjat.Puntuacio;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PuntuacioTest {
+class PuntuacioTest {
 
     private Puntuacio puntuacio;
 
-    @Test
-    void calcularPuntuacio(){
-        assertEquals();
+    @DisplayName("Dificultat del penjat")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4})
+    void mostrarDificultat(int dificultat){
     }
 
+
+    @DisplayName("Calcular la puntació")
+    @Test
+    void calcularPuntuacioString(String[] paraulaEndevinada){
+    assertTrue(String paraulaEndevinada);
+    }
 }
